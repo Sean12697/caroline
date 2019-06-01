282ceb4cea82fc2c1a176753d9f8065a02ff9ba5
Sean O'Mahoney <seandavidomahoney@gmail.com>
2019-06-01 17:20:02 +0100

    htm -> html

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ const api = "https://caroline-server.herokuapp.com/"; // user/Sean12697
 window.addEventListener("load", () => {
     let params = paramsToObj();
     reqData("GET", {}, `${api}user/${ params.user ? params.user : "Sean12697" }`, (data) => {
+        console.log(data);
         document.getElementById("name").innerHTML = data.user.name;
         document.getElementById("img").src = data.user.profile_image_url;
+        anychart.fromJson(lineChartTemplate("Sentiment Trend over Time", data.avgSentiments.map((value, time) => {
+            return {
+                x: time,
+                value: value
+            }
+        }), "avgSentiment")).draw();
     }, (err) => {});
 });
 
@@ -35,4 +42,20 @@ function paramsToObj() {
     let array = params.replace("?", "").split("=");
     for (let i = 0; i < array.length; i += 2) obj[array[i]] = array[i + 1];
     return obj;
+}
+
+function lineChartTemplate(text, data, container) {
+    return {
+        chart: {
+            type: "line",
+            title: {
+                text: text
+            },
+            series: [{
+                seriesType: "column",
+                data: data
+            }],
+            container: container
+        }
+    }
 }
